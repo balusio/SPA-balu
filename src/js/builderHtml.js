c5a32e0ad0eb94bd514cc6159768d471160a0a5c
Balu Manzano <jorgemanzanoweb@gmail.com>
2018-07-28 15:08:45 -0300

    added CORS and AXIOS call

--- src/js/builderHtml.js
+++ src/js/builderHtml.js
@@ -1,23 +1,21 @@
 'use strict';
 
-// Example 1: Creating a new class (declaration-form)
-// ===============================================================
+import axios from 'axios';
 
-// A base class is defined using the new reserved 'class' keyword
 export default class builder {
-  // ..and an (optional) custom class constructor. If one is
-  // not supplied, a default constructor is used instead:
-  // constructor() { }
+
   constructor(height, width) {
+    this.http = axios;
     this.name = 'Polygon';
     this.height = height;
     this.width = width;
     this.class = 'zalala';
   }
 
-  // Simple class instance methods using short-hand method
-  // declaration
   createElement() {
+    this.http.get('http://localhost/api/public/bd-test').then((response)=>{
+      console.log(response);
+    });
     console.log('Hi, I am a ', this.name + '.');
   }
 
@@ -26,6 +24,5 @@ export default class builder {
       'and gonia (angle).');
   }
 
-  // We will look at static and subclassed methods shortly
 }
 
