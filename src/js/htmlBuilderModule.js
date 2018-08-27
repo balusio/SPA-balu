0ae07ac07d5f4f0d274e3212a846da4eada5ad71
Jorge Manzano <jorge.manzano@globallogic.com>
2018-08-27 00:47:46 -0300

    added image file input

--- src/js/htmlBuilderModule.js
+++ src/js/htmlBuilderModule.js
@@ -1,42 +1,50 @@
 'use strict';
-
+import { enviroment } from './enviroment';
 export default class builderModule{
 
 	constructor(id,dataElement){
 		this.currentId =id;
 		this.name = dataElement.name;
 		this.description = dataElement.description;
+		this.imageFileUrl= dataElement.image;
 		this.containerDiv = document.createElement('form');
 		this.title_iput;
 		this.description_textarea;
 		this.editButton;
 		this.cancelButton;
+		this.image;
 	}
 
 	createItem(){
 		this.containerDiv.setAttribute('id', `form_${this.currentId}`);
 		this.containerDiv.setAttribute('data-sort', this.currentId);
 		this.containerDiv.setAttribute('class', 'mdc-card');
-		const titleDiv = document.createElement('div');
-		titleDiv.setAttribute('class', 'mdc-card__media mdc-card__media');
-		this.title_iput = document.createElement('input');
-		this.title_iput.setAttribute('class', 'demo-card__title mdc-typography--headline6 not-sortable');
-		this.title_iput.setAttribute('value', this.name);
-		this.title_iput.setAttribute('disabled', true);
-		titleDiv.appendChild(this.title_iput);
-		// description
-		this.description_textarea = document.createElement('textarea');
-		this.description_textarea.setAttribute('class', 'demo-card__secondary mdc-typography--body2 not-sortable');
-		this.description_textarea.value = this.description;
-		this.description_textarea.setAttribute('disabled', true);
-		titleDiv.appendChild(this.description_textarea);
-		this.containerDiv.appendChild(titleDiv);
-		//BUTONS
-		const buttonContainer = document.createElement('mdc-card__actions');
-		const buttonRow = document.createElement('mdc-card__action-buttons');
-		this.editButton = document.createElement('button');
-		this.editButton.setAttribute('class', 'mdc-button mdc-card__action mdc-card__action--button not-sortable');
-		const textAccept = document.createTextNode("Editar");
+		// <input id="photo" type="file" accept="image/*">  ;
+	   this.image = document.createElement('input');
+	   this.image.setAttribute('type','file');
+		this.image.setAttribute('style', `background-image=url(${ enviroment }public/${this.imageFileUrl});`);
+		this.image.setAttribute('value', `${this.imageFileUrl}`);
+	   const titleDiv = document.createElement('div');
+	   titleDiv.setAttribute('class', 'mdc-card__media mdc-card__media');
+	   titleDiv.appendChild(this.image);
+	   this.title_iput = document.createElement('input');
+	   this.title_iput.setAttribute('class', 'demo-card__title mdc-typography--headline6 not-sortable');
+	   this.title_iput.setAttribute('value', this.name);
+	   this.title_iput.setAttribute('disabled', true);
+	   titleDiv.appendChild(this.title_iput);
+	   // description
+	   this.description_textarea = document.createElement('textarea');
+	   this.description_textarea.setAttribute('class', 'demo-card__secondary mdc-typography--body2 not-sortable');
+	   this.description_textarea.value = this.description;
+	   this.description_textarea.setAttribute('disabled', true);
+	   titleDiv.appendChild(this.description_textarea);
+	   this.containerDiv.appendChild(titleDiv);
+	   //BUTONS
+	   const buttonContainer = document.createElement('mdc-card__actions');
+	   const buttonRow = document.createElement('mdc-card__action-buttons');
+	   this.editButton = document.createElement('button');
+	   this.editButton.setAttribute('class', 'mdc-button mdc-card__action mdc-card__action--button not-sortable');
+	   const textAccept = document.createTextNode("Editar");
 		this.editButton.onclick = (event)=>{
 			event.preventDefault();
 			this.editElement(event);
@@ -59,15 +67,16 @@ export default class builderModule{
 
 	}
 	editElement(event) {
-		if (event.target.innerHTML === 'Editar') {
+		if (this.editButton.innerHTML === 'Editar') {
 			
 			console.log(event.target);
-			event.target.innerHTML = 'aceptar';
+			this.editButton.innerHTML = 'ACEPTAR';
 			this.title_iput.removeAttribute('disabled');
-		} else if (event.target.innerHTML === 'aceptar') {
+			this.description_textarea.removeAttribute('disabled');
+		} else if (this.editButton.innerHTML === 'ACEPTAR') {
 			this.title_iput.setAttribute('disabled', true);
-			this.title_iput.setAttribute('disabled');
-			event.target.innerHTML = 'editar';
+			this.description_textarea.setAttribute('disabled',true);
+			this.editButton.innerHTML = 'Editar';
 		} else {
 		
 		}
